2c6ddbc2aea17ee5cd9f4eba37cf969682fef6fe
renann.gomes <renannpop11@gmail.com>
2024-12-23 22:01:14 -0300

    Fix error

--- 775. Global and Local Inversions/775.py
+++ 775. Global and Local Inversions/775.py
@@ -1,17 +1,17 @@
 from typing import List
 
 class Solution(object):
-    def isIdealPermutation(self, A: List[int]) -> bool:
+    def isIdealPermutation(self, nums):
         numLoc = 0
         numGlo = 0
 
-        for idx, num in enumerate(A):
+        for idx, num in enumerate(nums):
             if num > idx:
                 numGlo += (num-idx)
             elif num < idx:
                 numGlo += (idx-num-1)
             
-            if idx < len(A)-1 and num > A[idx+1]:
+            if idx < len(nums)-1 and num > nums[idx+1]:
                 numLoc += 1
         
         print(numLoc, numGlo)
